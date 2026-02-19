bf451ea6253c740f73ab66b0faa47468dda91fd6
Md Siyam <mdsiyam9424@gmail.com>
2026-02-19 14:53:27 +0600

    learning query and params deeply

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 const app = express();
+import fs from "fs";
+app.use(express.json());
 app.get("/", (req, res) => {
     res.json("I learning express with typescript");
 });
-app.get("/todos", (req, res) => {
-    res.json("this is to do server");
+app.get("/todos/:title/:body", (req, res) => {
+    console.log("from query", req.query);
+    console.log("from params", req.params);
+    const data = fs.readFileSync("./db/todo.json", { "encoding": "utf-8" });
+    res.json(data);
 });
 app.post("/todos/create-todos", (req, res) => {
     res.json("create to do");
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from "express"
 import type { Application,  Request,  Response } from "express"
 const app: Application = express()
+import fs from "fs"
 
+app.use(express.json())
 
 app.get("/", (req: Request, res:Response)=>{
    res.json("I learning express with typescript")
 })
-app.get("/todos", (req: Request, res:Response)=>{
-   res.json("this is to do server")
+app.get("/todos/:title/:body", (req: Request, res:Response)=>{
+   console.log("from query", req.query)
+   console.log("from params", req.params)
+   const data = fs.readFileSync("./db/todo.json", {"encoding": "utf-8"})
+   res.json(data)
 })
 app.post("/todos/create-todos", (req: Request, res:Response)=>{
    res.json("create to do")
